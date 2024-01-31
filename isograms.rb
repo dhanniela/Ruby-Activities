c10878a4dae6bdcfe2d8792a408d02e091cd6b44
dhanniela <tjmaureadhanniela@gmail.com>
2024-01-31 17:49:21 +0800

    refactor code

--- isograms.rb
+++ isograms.rb
@@ -1,12 +1,10 @@
 def is_isogram(str)
     str = str.downcase
-    unique_chars = Set.new
 
     str.each_char do |char|
-        if unique_chars.include?(char)
+        if str.count(char) > 1
             return false
         end
-        unique_chars.add(char)
     end
 
     return true
@@ -15,4 +13,24 @@ end
 puts is_isogram("Dermatoglyphics")
 puts is_isogram("aba")
 puts is_isogram("moOse")
+puts is_isogram("asdnjkahduiwandjksancjkasnbcjksncjksakndjkasndcjksanckjsancksancjkasndjkasldasmcsad")
 
+
+def is_isogram?(str)
+    str = str.downcase
+    unique_chars = Set.new
+
+    str.each_char do |char|
+        if unique_chars.include?(char)
+            return false
+        end
+        
+        unique_chars.add(char)
+    end
+
+    return true
+end
+
+puts is_isogram?("Dermatoglyphics")
+puts is_isogram?("aba")
+puts is_isogram?("moOse")
